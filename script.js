da8180cf0c181f4e9f676bc79628775009110345
rahmanhasanoglu <rahman.hasanoglu@gmail.com>
2018-07-06 13:27:38 +0200

    Go Live-1

---
+++ script.js
@@ -0,0 +1,11 @@
+$('#insights').on('click',function(){
+   const images = $('#images').position().top;
+    
+    $('html, body').animate({
+        
+        scrollTop:images
+        
+    },900   
+    );
+    
+});
